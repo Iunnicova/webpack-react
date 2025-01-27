30781126c5cdf2ebdb6761b325836486b45bbb2a
Yunnicova Valentina <yunnicova@yandex.ru>
2025-01-27 16:59:33 +0100

    feat: add connecting the reactor

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HTMLWebpackPlugins = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            //подключение других лоадеров
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: ['@svgr/webpack', 'url-loader'],
+              },
+
+            {
+                test: /\.(png|jpg|gif|webp)$/,
+                type: 'asset/resource',
+                    generator: {
+                        filename: 'static/images/[hash][ext][query]',
+                    },
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                    generator: {
+                        filename: 'static/fonts/[hash][ext][query]',
+                    },
+            },
+            
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                                auto: /\.module\.\w+$/i,
+                                namedExport: false,
+                            },
+                            importLoaders: 2, //Значение 2 говорит о том, что некоторые трансформации PostCSS нужно применить до css-loader.
+                        },
+                    },
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
+    },
+
+    plugins: [
+        new ReactRefreshWebpackPlugin(),
+        new HTMLWebpackPlugins({
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'static/styles/index.css',
+        }),
+    ],
+};
